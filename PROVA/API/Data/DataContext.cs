f740ce6d90b728d85229ad95b8e5c1594cefd1f7
zricardobraga <zericardobraga@gmail.com>
2021-09-28 19:31:41 -0300

    recriado o projeto

---
+++ PROVA/API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> builderOptions)
+            : base(builderOptions)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseInMemoryDatabase("conenctionString");
+        }
+    }
+}
--- PROVA/API/Startup.cs
+++ PROVA/API/Startup.cs
@@ -1,16 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace API
 {
@@ -26,20 +27,9 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(
-                options => { 
-                    options.AddPolicy("CorsPolicy", builder => builder
-                    .AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    );
-                }
-            );
-
             services.AddDbContext<DataContext>(
-                options => options.UseInMemoryDatabase("database")
-                );
-
+                options => options.UseInMemoryDatabase("connectionString")
+            );
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -57,10 +47,6 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
-
-            app.UseCors("CorsPolicy");
-            
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
