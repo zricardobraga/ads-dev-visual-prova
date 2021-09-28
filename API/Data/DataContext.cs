9cc8ac3bc4dfc3f39816fb9ee3637541b50d7280
zricardobraga <zericardobraga@gmail.com>
2021-09-28 18:51:34 -0300

    criada a web, instalado o entity framework in memory

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data 
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+		public DbSet<Musica> Musicas { get; set; }
+		
+	}
+
+}
---
+++ API/Models/Musica.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace API.Models
+{
+    public class Musica
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Artista { get; set; }
+
+        public string Album { get; set; }
+
+        public string Genero { get; set; }
+    }
+}
